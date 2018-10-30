3569bf4051067ad3f7692319feadfaa0e72b2382
Nicola.Simonetti <nicola.simonetti@dedagroup.it>
2018-10-30 12:29:14 +0100

    DataRow unique key

--- src/DataRow.js
+++ src/DataRow.js
@@ -1,14 +1,18 @@
 // @flow
 
+import uuidv4 from 'uuid/v4'
+
 export default class DataRow<T> {
 
   content: T
   type: 'element' | 'group-header' | 'aggregate'
   originalIndex: ?number
+  key: string
 
   constructor(content : T, type : 'element' | 'group-header' | 'aggregate', originalIndex: ?number) {
     this.content = content
     this.type = type
     this.originalIndex = originalIndex
+    this.key = uuidv4()
   }
 }
