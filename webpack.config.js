565629784f1abf6e02aaeafe56ff86ab4a58be49
Michele Lugano <michele.lugano9@gmail.com>
2018-11-06 14:13:17 +0100

    Removed splitting optimization

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,5 @@ config.devtool = 'cheap-module-source-map'
 config.plugins = config.plugins.push([
   new webpack.optimize.AggressiveMergingPlugin()
 ])
-config.optimization = {
-  splitChunks: {
-    chunks: 'all'
-  }
-}
 
 module.exports = config
